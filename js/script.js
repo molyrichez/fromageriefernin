466c10b6a7d212a053a8f78c7cc9884a539ec251
Moly <64364914+molyrichez@users.noreply.github.com>
2021-04-06 16:59:38 +0200

    Batch update

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
 
 
     window.silex = window.silex || {}
-    window.silex.data = {"site":{"width":1280},"pages":[{"id":"page-test","displayName":"test","link":{"linkType":"LinkTypePage","href":"#!page-test"},"canDelete":true,"canProperties":true,"canMove":true,"canRename":true,"opened":false}]}
+    window.silex.data = {"site":{"width":1280},"pages":[{"id":"page-home","displayName":"home","link":{"linkType":"LinkTypePage","href":"#!page-home"},"canDelete":true,"canProperties":true,"canMove":true,"canRename":true,"opened":false}]}
