20f80e148bdf36535b7317ab3907f33ac886277a
Moly <64364914+molyrichez@users.noreply.github.com>
2021-04-16 14:41:12 +0200

    Batch update

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@
     window.silex.data = {"site":{"width":1420},"pages":[{"id":"page-home","displayName":"home","link":{"linkType":"LinkTypePage","href":"#!page-home"},"canDelete":true,"canProperties":true,"canMove":true,"canRename":true,"opened":false},{"id":"page-demarche","displayName":"demarche","link":{"linkType":"LinkTypePage","href":"#!page-demarche"},"canDelete":true,"canRename":true,"canMove":true,"canProperties":true,"opened":false},{"id":"page-clients-pro","displayName":"clients-pro","link":{"linkType":"LinkTypePage","href":"#!page-clients-pro"},"canDelete":true,"canRename":true,"canMove":true,"canProperties":true,"opened":false},{"id":"page-actu","displayName":"actu","link":{"linkType":"LinkTypePage","href":"#!page-actu"},"canDelete":true,"canRename":true,"canMove":true,"canProperties":true,"opened":false},{"id":"page-404","displayName":"404","link":{"linkType":"LinkTypePage","href":"#!page-404"},"canDelete":true,"canRename":true,"canMove":true,"canProperties":true,"opened":false},{"id":"page-merci","displayName":"merci","link":{"linkType":"LinkTypePage","href":"#!page-merci"},"canDelete":true,"canRename":true,"canMove":true,"canProperties":true,"opened":false}]}
 /* pastille eshop rotate on scroll */
 $(function() {
-  elem.scrollRotate();
+  $('.rotate').scrollRotate()
 })
 
 /* parallawe avec texte */
